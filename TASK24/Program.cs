8a886528947499d914dc25aa415ed820cc3b3ae7
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-07 19:24:48 +0300

    Добавила TASK26

--- TASK24/Program.cs
+++ TASK24/Program.cs
@@ -5,29 +5,45 @@
 // 4 -> 10
 // 8 -> 36
 
-Console.WriteLine("Ведите число ");
+// Console.WriteLine("Ведите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sumResult = SumNumbers (number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {sumResult}");
+
+// int SumNumbers(int num)
+// {
+//     int sum = default;
+//     for (int i = 1; i <= num; i++) ;
+//     {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+
+// int SumNumbers2(int num)
+// {
+//     int sum = default;
+//     int counter = 1;
+//     while (counter <= num)
+//     {
+//         sum = sum + counter;
+//     }
+//     return sum;
+// }
+
+
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int sumResult = SumNumbers (number);
-Console.WriteLine($"Сумма чисел от 1 до {number} = {sumResult}");
+int sumResult = SumNumbers(number);
+Console.WriteLine($"Сумма чисел о 1 до {number} = {sumResult}");
 
 int SumNumbers(int num)
 {
     int sum = default;
-    for (int i = 0; i <= num; i++) ;
+    for (int i = 1; i <= num; i++)
     {
         sum = sum + i;
-    }
-    return sum;
-}
-
-
-int SumNumbers2(int num)
-{
-    int sum = default;
-    int counter = 1;
-    while (counter <= num)
-    {
-        sum = sum + counter;
     }
     return sum;
 }
