a371b376b555e2f76c53adaaa0cd293624d5fc4d
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-07 19:45:53 +0300

    Добавила TASK28

---
+++ TASK28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+int result = DigitMultiplication(number);
+Console.WriteLine($"Произведение числе от 1 до {number} = {result}");
+}
+else Console.WriteLine("Введено некорректное значение");
+
+int DigitMultiplication(int num)
+{
+int multiplic = 1;
+for (int i = 1; i <= num; i++)
+{
+multiplic = multiplic * i;
+}
+return multiplic;
+}
