40c3b8c8e22cbc217f1af9b38925a57d07ee8aff
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-09 18:11:19 +0300

    ДобавилаTASK29

---
+++ TASK29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая 
+// 1.задаёт массив из 8 элементов и 
+// 2.выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1, 9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
