2f25db81b3e1ce734c425b701a273a1af74579e2
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-12 19:55:47 +0300

    Добавила TASK34 TASK35 TASK37

---
+++ TASK35/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int QuantityElemInRange(int[] array, int min, int max)
+{
+    int counter = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max) counter++;
+    }
+    return counter;
+}
+
+int[] arr = CreateArrayRndInt(10, 0, 1000);
+PrintArray(arr);
+int quantityElemInRange = QuantityElemInRange(arr, 10, 99);
+Console.WriteLine(quantityElemInRange);
+// разобраться почему ошибка
