73955ba9fed3d8ae389e50ac527581472310c32c
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-19 18:58:16 +0300

    Добавила TASK49

---
+++ TASK49/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+System.Console.WriteLine();
+
+GetSquareEvenElem(array2D);
+PrintMatrix(array2D);
+
+// метод возведение чётных элементов массива в квадрат
+void GetSquareEvenElem(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2) //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2) //columns (1)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
