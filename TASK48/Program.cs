8f6a3635ed5c9dfe8662249041d10a60018f1b7d
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-19 08:35:10 +0300

    Добавила TASK48

---
+++ TASK48/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.WriteLine("Введите размер массива m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] myMatrix = CreateMatrixSumIndex(m, n);
+PrintMatrix(myMatrix);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
