42c27fa5043306e26d829e5281089f3a1da3c528
Ruymina <113090924+Ruymina@users.noreply.github.com>
2022-10-21 14:49:40 +0300

    ДобавилаTASK67

---
+++ TASK67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+int number = InputIntData("Введите число");
+int summ = SumOfDigits(number);
+Console.WriteLine(summ);
+
+int SumOfDigits(int num)
+{
+    int sum = num % 10;
+    if (num > 0) sum += SumOfDigits(num / 10);
+    return sum;
+}
